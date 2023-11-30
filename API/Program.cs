945e7e2f15f5f04331b6c9d4c4a7b35fd932ee67
Victor Prospero <viprospe@iurtsbrcsp.onmicrosoft.com>
2023-11-30 14:13:10 -0300

    Incluído método de upload para o azure storage

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,17 @@
-using System.Runtime.CompilerServices;
+using Azure.Storage.Blobs;
+using System.Text.RegularExpressions;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var connectionString = app.Configuration.GetConnectionString("DefaultConnection");
+var azureConnectionString = app.Configuration.GetValue<string>("AzureStorageConnectionString");
+
+
 var secrets = new Secrets();
+
 app.Configuration.GetSection("Secrets").Bind(secrets);
+
 app.MapGet("/", () => new
 {
     ConnectionString = connectionString,
@@ -13,7 +19,26 @@ app.MapGet("/", () => new
     ApiUrl = app.Configuration.GetValue<string>("ApiUrl")
 });
 
+app.MapPost("/", (Upload model) => new {
+    ImageUrl = UploadBase64Image(model.Image, "user-images")
+});
+
 app.Run();
+
+string UploadBase64Image(string base64Image, string container)
+{
+    var fileName = Guid.NewGuid().ToString() + ".jpg";
+    var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(base64Image, "");
+    byte[] imageBytes = Convert.FromBase64String(data);
+    var blobClient = new BlobClient(azureConnectionString, container, fileName);
+    using (var stream = new MemoryStream(imageBytes))
+    {
+        blobClient.Upload(stream);
+    }
+    return blobClient.Uri.AbsoluteUri;
+}
+
+public record Upload(string Image);
 
 public class Secrets
 {
